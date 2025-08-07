dc0e94bffcc14298a6ca1a667de27e969fe510e4
wayixia <wayixia@gmail.com>
2025-08-07 22:55:59 +0800

    dist image

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name lonyda -v /root/docker:/app -v /root/docker-db:/var/lib/mysql -p 443:443 -p 3306:3306 php-nginx:1.0
+docker run -d --name lonyda -v /root/docker:/app -v /root/docker-db:/var/lib/mysql -p 443:443 -p 3306:3306 php-nginx-dist:1.0
